c9a40f416ef5aa8a11a1442180cc124f78310aca
seungmileee <dltmdal0928@naver.com>
2022-08-22 22:37:05 +0900

    Updates

--- gatsby-browser.js
+++
@@ -1,3 +0,0 @@
-// import 'prismjs/themes/prism-tomorrow.css'
-import { defineCustomElements as deckDeckGoHighlightElement } from '@deckdeckgo/highlight-code/dist/loader'
-deckDeckGoHighlightElement()
